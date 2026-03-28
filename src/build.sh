64b8d8390c8024fbf6f9bfeea40867f930cdf16d
kissetfall <j@ergohaven.xyz>
2026-03-28 13:28:13 +0300

    fix: copy DejaVuSans.ttf font into wasm build for glyph fix

--- src/build.sh
+++ src/build.sh
@@ -22,6 +22,8 @@ cp ../../via-keymap-precompiled/via_keyboard_stack.json usr/local/via_keyboards.
 cp ../../vial-gui/src/main/resources/base/qmk_settings.json usr/local
 mkdir -p usr/local/i18n
 cp ../../vial-gui/src/main/resources/base/i18n/vial_ru.qm usr/local/i18n/
+mkdir -p usr/local/fonts
+cp ../../vial-gui/src/main/resources/base/fonts/DejaVuSans.ttf usr/local/fonts/
 cp ../../vial-gui/src/build/settings/base.json usr/local/build_settings.json
 cp -r ../../vial-gui/src/main/python/* usr/local/lib/python3.11
 cp ../simpleeval.py usr/local/lib/python3.11
